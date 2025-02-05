c124f7e295ea3860a96314057e3c70781082427b
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-05 12:58:27 +0100

    exercise

--- arithmetic.py
+++ arithmetic.py
@@ -1,6 +1,5 @@
-x = ("exponentiation")
-def myfunc():
-    x = ("**")
-    print(x)
-myfunc()
-print(x)
+given_distance = 10 # in km
+given_time = 2562 # in sec
+miles_in_kilometre = (given_distance/1.61)
+average_pace = (given_time/miles_in_kilometre)
+print(average_pace)
--- bitwise.py
+++ bitwise.py
@@ -1,3 +1,2 @@
-x = 68
-y = 2
-print (x >> y)
+
+print (abs)
