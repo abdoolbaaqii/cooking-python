71658255a68a82e00003e019ee20b051a546b667
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-05 13:45:37 +0100

    exercise no.5

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,6 @@
-given_kilometre = 10 # in km
+given_kilometre = 10 
 given_time = 42.7 # in min
-miles_in_kilometre = (given_kilometre/1.61)
-average_pace = (given_time/miles_in_kilometre) # in min per mile
-print(average_pace)
+time_in_hour = (42.7/60)
+mile_in_kilometre = (given_kilometre/1.61)
+average_speed = (mile_in_kilometre/time_in_hour)
+print(average_speed)
