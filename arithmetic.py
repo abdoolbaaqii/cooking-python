2109d1a2d5bb76ac27b1ffbb712083653d606c80
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-05 13:12:19 +0100

    exercise no.4

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,5 @@
-given_distance = 10 # in km
-given_time = 2562 # in sec
-miles_in_kilometre = (given_distance/1.61)
-average_pace = (given_time/miles_in_kilometre)
+given_kilometre = 10 # in km
+given_time = 42.7 # in min
+miles_in_kilometre = (given_kilometre/1.61)
+average_pace = (given_time/miles_in_kilometre) # in min per mile
 print(average_pace)
