5ad8e91d53d14fdce6b7703037e6e6a52c865fb8
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-12 13:03:57 +0100

    asdfghjkl

--- function.py
+++ function.py
@@ -2,12 +2,4 @@ def habit(food):
     print(f"I love {food}")
 
 
-# 1- functions that perform a task
-# 2- functions that return a value
-def make_habit(food):
-    return f"I love {food}"
-
-
-ABQ = make_habit("pounded yam")
-file = open("ABQ.REASON", "w")
-file.write(ABQ)
+habit("pounded yam")
