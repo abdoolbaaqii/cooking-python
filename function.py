31b4183e2e7854fc38e6bdcb8c1e25ebf435f95f
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-12 12:31:11 +0100

    function

---
+++ function.py
@@ -0,0 +1,7 @@
+def habit(sport, entertainment):
+    print("My name is ABQ")
+    print(f"I love {entertainment} and  {sport}")
+
+
+habit("playing football", "joking" )
+habit("jogging", "singing")
--- variable.py
+++
@@ -1,4 +0,0 @@
-import math 
-x = 42
-y = (math.cos(x)**2) + (math.sin(x)**2)
-print(y)
