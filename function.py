e843ad389588d1ae9c2ad4dad2edaec41a0931fb
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-02-19 11:18:48 +0100

    print twice

--- function.py
+++
@@ -1,8 +0,0 @@
-def mul(*players):
-    coach = 10
-    for player in players:
-        coach = player
-    return coach 
-
-
-print(mul(3, 17, 7, 4))
---
+++ newfunc.py
@@ -0,0 +1,8 @@
+def print_twice(string):
+    print(string)
+    print(string)
+    string = "playboy"
+    
+
+name = "ABQ"
+print_twice(name)
