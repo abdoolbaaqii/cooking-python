10e0d872198daf0d4b7673a0a63574e99e4ff367
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-03-04 13:57:38 +0100

    function second exercise

---
+++ exercise_2.py
@@ -0,0 +1,18 @@
+def triangle(string , number):
+    print(string * number)
+    
+    
+number = 1
+
+
+def first_seven_lines():
+    print('L' * number)
+    print('L' * 2)
+    print('L' * 3)
+    print('L' * 4)
+    print('L' * 5)
+    print('L' * 6)
+    print('L' * 7)
+    
+    
+first_seven_lines()
