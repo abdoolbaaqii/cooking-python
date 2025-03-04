6c19835c19597885eca9aef27a0d6c299ee06144
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-03-04 12:19:05 +0100

    function exercise 1

---
+++ exercise.py
@@ -0,0 +1,10 @@
+def print_right(text):
+    print(text)
+    print(text1)
+    print(text2)
+    
+
+text = "          Monty"
+text1 = "          Python's"
+text2 = "          Flying circus"
+print_right(text)
