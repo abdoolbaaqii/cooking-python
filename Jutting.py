469d17a276699a8b9c3731754bc8a98424ad82d4
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-05-07 10:17:35 +0100

    arithmetic exercise

--- Jutting.py
+++ Jutting.py
@@ -7,8 +7,8 @@ one_mile = 1.61 # in km
 miles_in_10_km = (given_distance/one_mile)
 print(miles_in_10_km)
 
-total_time_in_min = 42.7
-total_time_in_sec = (42*60) + (0.7*60)
+# total time given = 42 min 42 sec
+total_time_in_sec = (42*60) + (42)
 average_pace = (total_time_in_sec/miles_in_10_km)
 print(average_pace)
 
