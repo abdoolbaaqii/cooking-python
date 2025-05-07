215939eeeba6a4826ec955871534bde0407e596e
abdoolbaaqii <abdoolghaniabdoolbaaqii77@gmail.com>
2025-05-07 11:49:05 +0100

    exercise, part 1 and 2

---
+++ variable.py
@@ -0,0 +1,10 @@
+from math import pi, cos, sin 
+radius = 5 # in centimeter
+volume_of_a_sphere = (4/3)*(pi)*radius**3 # measured in cubic centimeter 
+print(volume_of_a_sphere)
+
+x = 42
+trigonometry_rule = (sin(x)**2) + (cos(x)**2)
+print(trigonometry_rule)
+
+
